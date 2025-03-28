537b15f4bdf18634ba053a8fb592ef32c7470a80
Luke Hamann <135341515+luke-hamann@users.noreply.github.com>
2025-03-28 07:09:22 -0500

    Add step 11.

--- ClassSchedule/TagHelpers/SubmitButtonTagHelper.cs
+++ ClassSchedule/TagHelpers/SubmitButtonTagHelper.cs
@@ -5,5 +5,9 @@ namespace ClassSchedule.TagHelpers
     [HtmlTargetElement("button", Attributes = "[type=submit]")]
     public class SubmitButtonTagHelper : TagHelper
     {
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.Attributes.AppendCssClass("btn btn-dark");
+        }
     }
 }
