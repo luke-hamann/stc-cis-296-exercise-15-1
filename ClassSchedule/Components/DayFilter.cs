4ab35b31ae203be29230a6385ffdadf49cec71b8
Luke Hamann <135341515+luke-hamann@users.noreply.github.com>
2025-03-28 08:51:46 -0500

    Add step 25.

--- ClassSchedule/Components/DayFilter.cs
+++ ClassSchedule/Components/DayFilter.cs
@@ -1,8 +1,11 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ClassSchedule.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace ClassSchedule.Components
 {
     public class DayFilter : ViewComponent
     {
+        private IRepository<Day> days { get; set; }
+        public DayFilter(IRepository<Day> rep) => days = rep;
     }
 }
